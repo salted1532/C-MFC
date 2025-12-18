af4b4bddf8f88f9bd6cadfdceb99c539fb0106b0
Salted1532 <64358271+salted1532@users.noreply.github.com>
2025-12-18 16:40:29 +0900

    14주차 수업내용4

---
+++ 14주차/MFCApplication3/MFCApplication3/MFCApplication3Dlg.cpp
@@ -0,0 +1,302 @@
+﻿
+// MFCApplication3Dlg.cpp: 구현 파일
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication3.h"
+#include "MFCApplication3Dlg.h"
+#include "afxdialogex.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// 응용 프로그램 정보에 사용되는 CAboutDlg 대화 상자입니다.
+
+class CAboutDlg : public CDialogEx
+{
+public:
+	CAboutDlg();
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_ABOUTBOX };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+
+// 구현입니다.
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedButton3();
+};
+
+CAboutDlg::CAboutDlg() : CDialogEx(IDD_ABOUTBOX)
+{
+}
+
+void CAboutDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+}
+
+BEGIN_MESSAGE_MAP(CAboutDlg, CDialogEx)
+
+END_MESSAGE_MAP()
+
+
+// CMFCApplication3Dlg 대화 상자
+
+
+
+CMFCApplication3Dlg::CMFCApplication3Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION3_DIALOG, pParent)
+	, m_auto(FALSE)
+	, m_update(FALSE)
+	, radio1(0)
+	, m_spinnum(0)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication3Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Check(pDX, IDC_AUTO, m_auto);
+	DDX_Check(pDX, IDC_UPDATE, m_update);
+	DDX_Radio(pDX, IDC_RADIO1, radio1);
+	DDX_Control(pDX, IDC_PROGRESS1, m_slider);
+	DDX_Control(pDX, IDC_SLIDER1, m_scroll);
+	DDX_Control(pDX, IDC_SPIN2, m_spin);
+	DDX_Text(pDX, IDC_EDIT2, m_spinnum);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication3Dlg, CDialogEx)
+	ON_WM_SYSCOMMAND()
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_BN_CLICKED(IDC_BUTTON2, &CMFCApplication3Dlg::OnBnClickedButton2)
+	ON_BN_CLICKED(IDC_AUTO, &CMFCApplication3Dlg::OnBnClickedAuto)
+	ON_BN_CLICKED(IDC_BUTTON1, &CMFCApplication3Dlg::OnBnClickedButton1)
+	ON_BN_CLICKED(IDC_RADIO1, &CMFCApplication3Dlg::OnBnClickedRadio1)
+	ON_BN_CLICKED(IDC_BUTTON3, &CMFCApplication3Dlg::OnBnClickedButton3)
+	ON_BN_CLICKED(IDC_BUTTON4, &CMFCApplication3Dlg::OnBnClickedButton4)
+	ON_BN_CLICKED(IDC_BUTTON5, &CMFCApplication3Dlg::OnBnClickedButton5)
+	ON_NOTIFY(NM_RELEASEDCAPTURE, IDC_SLIDER1, &CMFCApplication3Dlg::OnNMReleasedcaptureSlider1)
+	ON_NOTIFY(NM_THEMECHANGED, IDC_SPIN2, &CMFCApplication3Dlg::OnNMThemeChangedSpin2)
+	ON_EN_CHANGE(IDC_EDIT2, &CMFCApplication3Dlg::OnEnChangeEdit2)
+END_MESSAGE_MAP()
+
+
+// CMFCApplication3Dlg 메시지 처리기
+
+BOOL CMFCApplication3Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+
+	// 시스템 메뉴에 "정보..." 메뉴 항목을 추가합니다.
+
+	// IDM_ABOUTBOX는 시스템 명령 범위에 있어야 합니다.
+	ASSERT((IDM_ABOUTBOX & 0xFFF0) == IDM_ABOUTBOX);
+	ASSERT(IDM_ABOUTBOX < 0xF000);
+
+	CMenu* pSysMenu = GetSystemMenu(FALSE);
+	if (pSysMenu != nullptr)
+	{
+		BOOL bNameValid;
+		CString strAboutMenu;
+		bNameValid = strAboutMenu.LoadString(IDS_ABOUTBOX);
+		ASSERT(bNameValid);
+		if (!strAboutMenu.IsEmpty())
+		{
+			pSysMenu->AppendMenu(MF_SEPARATOR);
+			pSysMenu->AppendMenu(MF_STRING, IDM_ABOUTBOX, strAboutMenu);
+		}
+	}
+
+	// 이 대화 상자의 아이콘을 설정합니다.  응용 프로그램의 주 창이 대화 상자가 아닐 경우에는
+	//  프레임워크가 이 작업을 자동으로 수행합니다.
+	SetIcon(m_hIcon, TRUE);			// 큰 아이콘을 설정합니다.
+	SetIcon(m_hIcon, FALSE);		// 작은 아이콘을 설정합니다.
+
+	// TODO: 여기에 추가 초기화 작업을 추가합니다.
+		m_slider.SetRange(0, 100);
+
+	return TRUE;  // 포커스를 컨트롤에 설정하지 않으면 TRUE를 반환합니다.
+}
+
+void CMFCApplication3Dlg::OnSysCommand(UINT nID, LPARAM lParam)
+{
+	if ((nID & 0xFFF0) == IDM_ABOUTBOX)
+	{
+		CAboutDlg dlgAbout;
+		dlgAbout.DoModal();
+	}
+	else
+	{
+		CDialogEx::OnSysCommand(nID, lParam);
+	}
+}
+
+// 대화 상자에 최소화 단추를 추가할 경우 아이콘을 그리려면
+//  아래 코드가 필요합니다.  문서/뷰 모델을 사용하는 MFC 애플리케이션의 경우에는
+//  프레임워크에서 이 작업을 자동으로 수행합니다.
+
+void CMFCApplication3Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 클라이언트 사각형에서 아이콘을 가운데에 맞춥니다.
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 아이콘을 그립니다.
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+// 사용자가 최소화된 창을 끄는 동안에 커서가 표시되도록 시스템에서
+//  이 함수를 호출합니다.
+HCURSOR CMFCApplication3Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+void CMFCApplication3Dlg::OnBnClickedButton2()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	m_auto = true;
+	m_update = false;
+	UpdateData(false);
+}
+
+
+void CMFCApplication3Dlg::OnBnClickedAuto()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData();
+	if (m_auto) {
+		GetDlgItem(IDC_UPDATE)->EnableWindow(FALSE);
+	}
+	else {
+		GetDlgItem(IDC_UPDATE)->EnableWindow(TRUE)	;
+	}
+}
+
+void CMFCApplication3Dlg::OnBnClickedButton1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData();
+	CString strTmp, strTmp1, strTmp2;
+	if (m_auto == 1) {
+		GetDlgItemText(IDC_AUTO, strTmp1);
+	}
+	if (m_update == 1) {
+		GetDlgItemText(IDC_UPDATE, strTmp2);
+	}
+
+	strTmp.Format(_T("AUTO = %s, UPDATE = %s"), strTmp1, strTmp2);
+
+	AfxMessageBox(strTmp);
+}
+
+
+void CMFCApplication3Dlg::OnBnClickedButton3() {
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData();
+	CString strTmp;
+	if (radio1 ==  0) {
+		GetDlgItemText(IDC_RADIO1, strTmp);
+	}
+	else if (radio1 == 1) {
+		GetDlgItemText(IDC_RADIO2, strTmp);
+	}
+	else if (radio1 == 2) {
+		GetDlgItemText(IDC_RADIO3, strTmp);
+	}
+
+	AfxMessageBox(strTmp);
+}
+
+void CMFCApplication3Dlg::OnBnClickedRadio1()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+}
+
+
+void CMFCApplication3Dlg::OnBnClickedButton4()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	static int num = 0;
+	num += 10;
+	m_slider.SetPos(num);   
+
+	int num2 = m_spin.GetPos();
+	m_slider.SetPos(num2);
+}
+
+
+void CMFCApplication3Dlg::OnBnClickedButton5()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(true);
+	int num = m_scroll.GetPos();
+	m_slider.SetPos(num);
+	UpdateData(false);
+}
+
+
+void CMFCApplication3Dlg::OnNMReleasedcaptureSlider1(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+		UpdateData(true);
+	int num = m_scroll.GetPos();
+	m_slider.SetPos(num);
+	UpdateData(false);
+	*pResult = 0;
+}
+
+
+void CMFCApplication3Dlg::OnNMThemeChangedSpin2(NMHDR* pNMHDR, LRESULT* pResult)
+{
+	// 이 기능을 사용하려면 Windows XP 이상이 필요합니다.
+	// _WIN32_WINNT 기호는 0x0501보다 크거나 같아야 합니다.
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+
+	*pResult = 0;
+}
+
+
+void CMFCApplication3Dlg::OnEnChangeEdit2()
+{
+	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+	// CDialogEx::OnInitDialog() 함수를 재지정 
+	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+	// 이 알림 메시지를 보내지 않습니다.
+
+	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
+		UpdateData(true);
+		int num2 = m_spinnum;
+	m_slider.SetPos(num2);
+	UpdateData(false);
+}
---
+++ 14주차/MFCApplication3/MFCApplication3/MFCApplication3Dlg.h
@@ -0,0 +1,52 @@
+﻿
+// MFCApplication3Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCApplication3Dlg 대화 상자
+class CMFCApplication3Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCApplication3Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCAPPLICATION3_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	BOOL m_auto;
+	BOOL m_update;
+	afx_msg void OnBnClickedButton2();
+	afx_msg void OnBnClickedAuto();
+	afx_msg void OnBnClickedButton3();
+	afx_msg void OnBnClickedButton1();
+	afx_msg void OnBnClickedRadio1();
+	int radio1;
+	CProgressCtrl m_slider;
+	afx_msg void OnBnClickedButton4();
+	CSliderCtrl m_scroll;
+	afx_msg void OnBnClickedButton5();
+	afx_msg void OnNMReleasedcaptureSlider1(NMHDR* pNMHDR, LRESULT* pResult);
+	CSpinButtonCtrl m_spin;
+	afx_msg void OnNMThemeChangedSpin2(NMHDR* pNMHDR, LRESULT* pResult);
+	afx_msg void OnEnChangeEdit2();
+	int m_spinnum;
+};
